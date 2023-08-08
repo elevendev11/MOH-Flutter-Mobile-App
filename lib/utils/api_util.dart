c5018b2b02554ac514ac37641b98e3bc3950d3f8
Abdullah Masood <amasood@octans.ai>
2023-08-08 10:09:38 +0500

    changes in the api util

--- lib/utils/api_util.dart
+++ lib/utils/api_util.dart
@@ -10,34 +10,34 @@ class ApiUtil {
     });
   }
 
-  static final String _baseUrlV1 = 'http://aliz31.sg-host.com';
+  static const String _baseUrlV1 = 'http://aliz31.sg-host.com';
 
-  static final String _saBaseUrlV1 = '$_baseUrlV1/api';
-  static final String loginEndPoint = '$_saBaseUrlV1/login';
-  static final String registerEndPoint = '$_saBaseUrlV1/register';
-  static final String logoutEndPoint = '$_saBaseUrlV1/logout';
-  static final String updateUserEndPoint = '$_saBaseUrlV1/update_user/';
-  static final String categoriesEndPoint = '$_saBaseUrlV1/categories';
-  static final String assesmentEndPoint = '$_saBaseUrlV1/assesments';
-  static final String assesmentCreateEndPoint = '$_saBaseUrlV1/assesment_answer';
-  static final String successIndexEndPoint = '$_saBaseUrlV1/fetch_success_index/';
-  static final String happinessIndexEndPoint = '$_saBaseUrlV1/fetch_happiness_index/';
-  static final String happinessIndexResultEndPoint = '$_saBaseUrlV1/happiness_index_result/';
-  static final String happinessIndexLineChartResponse = '$_saBaseUrlV1/happiness_index_line_chart_response/';
-  static final String evaluationTypeIntellectEndPoint = '$_saBaseUrlV1/fetch_evaluation_type_intellect/';
-  static final String evaluationTypeIntellectResultEndPoint = '$_saBaseUrlV1/evaluation_type_intellect_result/';
-  static final String evaluationTypeMindEndPoint = '$_saBaseUrlV1/fetch_evaluation_type_mind/';
-  static final String successIndexSubmitAnswerEndPoint = '$_saBaseUrlV1/success_index_answer';
-  static final String happinessIndexSubmitAnswerEndPoint = '$_saBaseUrlV1/happiness_index_answer';
-  static final String evaluationTypeIntellectSubmitAnswerEndPoint = '$_saBaseUrlV1/evaluation_type_intellect_answer';
-  static final String evaluationTypeMindSubmitAnswerEndPoint = '$_saBaseUrlV1/evaluation_type_mind_answer';
-  static final String successIndexResultEndPoint = '$_saBaseUrlV1/success_index_result/';
-  static final String evaluationTypeMindResultEndPoint = '$_saBaseUrlV1/evaluation_type_mind_result/';
-  static final String evaluationTypeMindQuestionResultEndPoint = '$_saBaseUrlV1/evaluation_type_mind_question_result/';
-  static final String successIndexLineChartResponse = '$_saBaseUrlV1/success_index_line_chart_response/';
-  static final String successIndexQuestionResult = '$_saBaseUrlV1/success_index_low_question_analytics/';
-  static final String happinessIndexQuestionResult = '$_saBaseUrlV1/happiness_index_low_question_analytics/';
-  static final String logsEndPoint = '$_saBaseUrlV1/logs/';
+  static const String _saBaseUrlV1 = '$_baseUrlV1/api';
+  static const String loginEndPoint = '$_saBaseUrlV1/login';
+  static const String registerEndPoint = '$_saBaseUrlV1/register';
+  static const String logoutEndPoint = '$_saBaseUrlV1/logout';
+  static const String updateUserEndPoint = '$_saBaseUrlV1/update_user/';
+  static const String categoriesEndPoint = '$_saBaseUrlV1/categories';
+  static const String assesmentEndPoint = '$_saBaseUrlV1/assesments';
+  static const String assesmentCreateEndPoint = '$_saBaseUrlV1/assesment_answer';
+  static const String successIndexEndPoint = '$_saBaseUrlV1/fetch_success_index/';
+  static const String happinessIndexEndPoint = '$_saBaseUrlV1/fetch_happiness_index/';
+  static const String happinessIndexResultEndPoint = '$_saBaseUrlV1/happiness_index_result/';
+  static const String happinessIndexLineChartResponse = '$_saBaseUrlV1/happiness_index_line_chart_response/';
+  static const String evaluationTypeIntellectEndPoint = '$_saBaseUrlV1/fetch_evaluation_type_intellect/';
+  static const String evaluationTypeIntellectResultEndPoint = '$_saBaseUrlV1/evaluation_type_intellect_result/';
+  static const String evaluationTypeMindEndPoint = '$_saBaseUrlV1/fetch_evaluation_type_mind/';
+  static const String successIndexSubmitAnswerEndPoint = '$_saBaseUrlV1/success_index_answer';
+  static const String happinessIndexSubmitAnswerEndPoint = '$_saBaseUrlV1/happiness_index_answer';
+  static const String evaluationTypeIntellectSubmitAnswerEndPoint = '$_saBaseUrlV1/evaluation_type_intellect_answer';
+  static const String evaluationTypeMindSubmitAnswerEndPoint = '$_saBaseUrlV1/evaluation_type_mind_answer';
+  static const String successIndexResultEndPoint = '$_saBaseUrlV1/success_index_result/';
+  static const String evaluationTypeMindResultEndPoint = '$_saBaseUrlV1/evaluation_type_mind_result/';
+  static const String evaluationTypeMindQuestionResultEndPoint = '$_saBaseUrlV1/evaluation_type_mind_question_result/';
+  static const String successIndexLineChartResponse = '$_saBaseUrlV1/success_index_line_chart_response/';
+  static const String successIndexQuestionResult = '$_saBaseUrlV1/success_index_low_question_analytics/';
+  static const String happinessIndexQuestionResult = '$_saBaseUrlV1/happiness_index_low_question_analytics/';
+  static const String logsEndPoint = '$_saBaseUrlV1/logs/';
 
-  static final String profileImageEndPoint = '$_baseUrlV1/public/storage/users';
+  static const String profileImageEndPoint = '$_baseUrlV1/public/storage/users';
 }
